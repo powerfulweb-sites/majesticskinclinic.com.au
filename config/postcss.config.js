50ba8c97c7839951e6777bb194d613d927631a30
sean-pwd <sean@powerfulwebdesign.com.au>
2021-10-07 21:09:02 +1100

    fix: disable postcss

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,30 +1,34 @@
-const autoprefixer = require('autoprefixer'); 
-const purgecss = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+const autoprefixer = require('autoprefixer');
+// const purgecss = require('@fullhuman/postcss-purgecss');
+// const whitelister = require('purgecss-whitelister');
 
 module.exports = {
   plugins: [
     autoprefixer(),
-    purgecss({
-      content: [
-        './layouts/**/*.html',
-        './content/**/*.md',
-      ],
-      safelist: [
-        'lazyloaded',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        'h3',
-        ...whitelister([
-          // './assets/scss/components/_buttons.scss',
-          './assets/scss/components/_code.scss',
-          // './assets/scss/components/_syntax.scss',
-        ]),
-      ],
-    }),
+    // purgecss({
+    //   content: [
+    //     './layouts/**/*.html',
+    //     './content/**/*.md',
+    //   ],
+    //   safelist: {
+    //     greedy: [
+    //       'lazyloaded',
+    //       'table',
+    //       'thead',
+    //       'tbody',
+    //       'tr',
+    //       'th',
+    //       'td',
+    //       'h3',
+    //       'aos-animate',
+    //       ...whitelister([
+    //         './assets/scss/vendors.scss',
+    //         // './assets/scss/components/_buttons.scss',
+    //         './assets/scss/components/_code.scss',
+    //         // './assets/scss/components/_syntax.scss',
+    //       ]),
+    //     ],
+    //   },
+    //}),
   ],
 }
